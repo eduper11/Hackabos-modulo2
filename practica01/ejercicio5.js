b084ab5b2f550fa7c36402d82c2cebd5121186fb
eduper11 <eduper11@gmail.com>
2019-03-10 20:41:31 +0100

    ejercicio5solucionado

--- practica01/ejercicio5.js
+++ practica01/ejercicio5.js
@@ -3,12 +3,9 @@
 pepe: for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
     // console.log(`coordenada ${i}${j}`);
-    coordenada = prompt(`Escoge 1 o 0 para la coordenada ${i},${j}`);
-    // if (coordenada == null || coordenada == "") {
-    //   alert("Has introducido un valor erroneo");
-    //   break pepe;
-    if (isFinite(coordenada) && coordenada == 1) {
-    } else if (isFinite(coordenada) && coordenada == 0) {
+    coordenada = +prompt(`Escoge 1 o 0 para la coordenada ${i},${j}`);
+    if (coordenada === 1) {
+    } else if (coordenada === 0) {
     } else {
       alert("Has introducido un valor erroneo");
       break pepe;
